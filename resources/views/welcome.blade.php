cfad08ea23319de764c1205dcb3a5fac571cc6b8
MOSAN <mochammadichsan02@gmail.com>
2023-09-17 10:28:17 +0700

    Update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -149,7 +149,7 @@
 
     <div class="modal fade" id="tambahModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
         aria-hidden="true">
-        <div class="modal-dialog" role="document">
+        <div class="modal-dialog" role="document" style="color: rgb(6, 6, 6); ">
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title" id="exampleModalLabel">Tambah Data Atlit</h5>
